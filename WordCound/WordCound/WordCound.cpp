87a2e557a75ef3d1a3f1f1fb8ce4dfe89005e5e4
zmx <1975625248@qq.com>
2020-05-21 23:42:12 +0800

    The end

--- WordCound/WordCound/WordCound.cpp
+++ WordCound/WordCound/WordCound.cpp
@@ -1,20 +1,95 @@
-﻿// WordCound.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include<stdio.h>
+#pragma warning(disable:4996);
+#define SIZE 10240 //数组空间要足够大，才可以尽可能的装下目标文件
 
-#include <iostream>
-
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	int ch, n = 0, i = 0, len = 0, word_val = 0;
+	FILE* fp;
+	char str[SIZE];
+
+	//检测命令行参数输入是否完整
+	if (argc != 3)
+	{
+		printf("Sorry,Input error\n");
+		return -1;
+	}
+
+	//检测将执行的功能是什么
+	if (argv[1][0] == '-')
+	{
+		if (argv[1][1] == 'w')
+		{
+			//以下为测定单词数代码
+			//打开目标文件
+			if ((fp = fopen(argv[2], "r")) == NULL)
+			{
+				printf("File open failed\n");
+				return -1;
+			}
+
+			//逐字节取出存在str数组中
+			while (!feof(fp))
+			{
+				ch = fgetc(fp);
+				if (ch != EOF)
+				{
+					str[i] = ch;
+					i++;
+				}
+			}
+			len = i;
+			str[i] = 0;
+			fclose(fp);
+
+			//遍历str数组确定单词数
+			//通过同wora_val来保证每个单词是完整计算
+			for (i = 0; i < len + 1; i++)
+			{
+				if (str[i] != ' ' && str[i] != ',')
+					word_val = 1;
+				if (str[i] == ' ' || str[i] == ',')
+				{
+					n += word_val;
+					word_val = 0;
+				}
+			}
+			n += word_val;
+			printf("单词个数:%d\n", n);
+		}
+		else if (argv[1][1] == 'c')
+		{
+			//以下为测定字符数的代码
+			//打开目标文件
+			if ((fp = fopen(argv[2], "r")) == NULL)
+			{
+				printf("File open failed\n");
+				return -1;
+			}
+
+			//逐字节取出存在str数组中
+			while (!feof(fp))
+			{
+				ch = fgetc(fp);
+				if (ch != EOF)
+				{
+					str[i] = ch;
+					i++;
+				}
+			}
+			fclose(fp);
+			len = i;
+
+			printf("字符个数%d\n", len);
+
+		}
+		else
+		{
+			//不能识别具体功能的，将拒绝执行
+			printf("暂不支持的此功能\n");
+			return -1;
+		}
+	}
+
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
